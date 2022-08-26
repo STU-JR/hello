9e8d033dcfedeb9094cbed6dc68d6018f66fbc55
STU-JR <jreiners3@huskers.unl.edu>
2022-08-26 23:59:47 +0000

    fixed standard libraries

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jenna Reiners
+ * Date: 2022/08/26
+ * Basic Hello World Program
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+    printf("Hello, I'm Jenna Reiners!\n");
+    printf("Major is Computer Science!\n");
+    printf("Minor is Math.\n");
+
+    return 0;
+}
